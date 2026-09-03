57f6cc139aa4b9352bba4073ca35a71150bad528
unknown <alifmuzakki1110@gmail.com>
2026-09-03 15:05:59 +0700

    perf(config): add sharp and configure image and package optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 480, 640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "idn-finlogos"],
+  },
 };
 
 export default nextConfig;
